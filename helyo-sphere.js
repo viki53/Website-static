f1a5ec61fd2a0ffb2b59d080ba9604f5658e14f5
Corentin Hatte <contact@corentin-hatte.eu>
2020-05-14 12:20:28 +0200

    Remove opacity effect on hero mousemove

--- helyo-sphere.js
+++ helyo-sphere.js
@@ -1,24 +1,9 @@
 'use strict';
 
-const heroOpacityProp = '--opacity-hero';
-
 document.addEventListener('DOMContentLoaded', () => {
-	const hero = document.getElementById('hero');
-
-	hero.addEventListener('mousemove', (event) => {
-		const center = {
-			x: hero.offsetWidth / 2,
-			y: hero.offsetHeight / 2
-		}
-
-		const maxDist = Math.sqrt(Math.pow(center.x, 2) + Math.pow(center.y, 2));
-
-		const distance = Math.sqrt(Math.pow(Math.abs(center.x - event.x), 2) + Math.pow(Math.abs(center.y - event.y), 2));
-
-		hero.style.setProperty(heroOpacityProp, 0.75 + ((distance / maxDist) / 4));
-	})
+	const hero = document.getElementById('hero')
 
 	hero.addEventListener('click', () => {
-		hero.classList.toggle('orange');
+		hero.classList.toggle('orange')
 	})
 })
