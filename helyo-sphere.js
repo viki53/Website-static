dc8582e092dee122afaf87dda97adc242f4b70b9
Corentin Hatte <contact@corentin-hatte.eu>
2021-01-25 11:44:46 +0100

    Use proper Jekyll layout

--- helyo-sphere.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-document.addEventListener('DOMContentLoaded', () => {
-	const hero = document.getElementById('hero')
-
-	hero.addEventListener('click', () => {
-		hero.classList.toggle('orange')
-	})
-})
